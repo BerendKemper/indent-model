47437e1e112adcdaf257e41f3ca7abbe0b43a887
Berend Kemper <berend_kemper@hotmail.com>
2021-03-07 10:57:42 +0100

    v1.0.4 migration class private properties

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 "use strict";
-// Object.assign getter:
-// cons: very slow initialize time
-// pros: fastest property access time, readable only
-// For logging strings dozen times a second prioritizes property access time
 const preSpaces = {};
-/**Configure your desired indentation model and put spaces with fixed tabsizes in between strings
- * @param {Object} options
- * @param {Number} options.tabSize
- * @param {Number} options.smallestSpace*/
-function IndentModel({ tabSize = 4, smallestSpace = 2 } = {}) {
-	if (this instanceof IndentModel === false)
-		throw TypeError(`Class constructors cannot be invoked without 'new'`);
-	if (smallestSpace > tabSize)
-		throw new Error("smallestSpace cannot be bigger than tabSize");
-	if (smallestSpace < 1)
-		throw new Error("smallestSpace must be bigger than 0");
-	const tooMuchSpace = smallestSpace + tabSize;
-	let preSpace = "";
-	for (let i = 1; i < smallestSpace; i++)
-		preSpace += " ";
-	for (let i = smallestSpace; i < tooMuchSpace; i++) {
-		preSpace += " ";
-		preSpaces[i] = preSpace;
-	}
-	Object.assign(this, {
-		get tabSize() {
-			return tabSize;
-		},
-		get smallestSpace() {
-			return smallestSpace;
+class IndentModel {
+	#tabSize = 4;
+	#smallestSpace = 2;
+	/**
+	 * Configure your desired indentation model and put spaces with fixed tabsizes in between strings
+	 * @param {Object} options
+	 * @param {Number} options.tabSize
+	 * @param {Number} options.smallestSpace
+	 **/
+	constructor({ tabSize, smallestSpace } = {}) {
+		if (typeof tabSize !== "number")
+			throw new Error("tabSize must be a number.");
+		if (typeof smallestSpace !== "number")
+			throw new Error("smallestSpace must be a number.");
+		if (smallestSpace > tabSize)
+			throw new Error("smallestSpace cannot be bigger than tabSize.");
+		if (smallestSpace < 1)
+			throw new Error("smallestSpace must be bigger than 0.");
+		const tooMuchSpace = smallestSpace + tabSize;
+		let preSpace = "";
+		for (let i = 1; i < smallestSpace; i++)
+			preSpace += " ";
+		for (let i = smallestSpace; i < tooMuchSpace; i++) {
+			preSpace += " ";
+			preSpaces[i] = preSpace;
 		}
-	});
-};
-IndentModel.prototype = {
+		this.#tabSize = tabSize;
+		this.#smallestSpace = smallestSpace;
+	};
+	/**
+	 * Turn the data input into a tabified string.
+	 * @param  {...any} data 
+	 * @returns 
+	 */
 	tabify(...data) {
 		const lastData = data.length - 1;
-		const tabSize = this.tabSize;
-		const smallestSpace = this.smallestSpace;
+		const tabSize = this.#tabSize;
+		const smallestSpace = this.#smallestSpace;
 		let tabified = "";
 		for (let i = 0; i < lastData; i++) {
 			const dataStr = String(data[i]);
@@ -49,6 +50,6 @@ IndentModel.prototype = {
 		}
 		tabified += data[lastData];
 		return tabified;
-	}
+	};
 };
 module.exports = IndentModel;
